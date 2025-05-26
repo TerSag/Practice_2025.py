796a366b74d6658700358a2961d2c8a83b38d40f
TerSag <sashka.tereshuk@gmail.com>
2025-05-26 23:19:56 +0300

    Initial commit

---
+++ my_logger.py
@@ -0,0 +1,11 @@
+import sys
+import datetime
+
+def log(message: str):
+
+    timestamp = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    error_message = f"[{timestamp}] {message}\n"
+    sys.stderr.write(error_message)
+
+if __name__ == "__main__":
+    log("Це повідомлення логу йде у stderr.")
